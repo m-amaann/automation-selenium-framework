c3481bcf4373d1dd7684be67bcc742b23a2f2c71
Mohamed Amaan <amaan26.rc@gmail.com>
2025-08-06 13:05:30 +0530

    added docker files

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,15 @@
-FROM ubuntu:latest
-LABEL authors="amaan"
-
 ENTRYPOINT ["top", "-b"]
+# Use official Maven with JDK image
+FROM maven:3.9.6-eclipse-temurin-21
+
+# Set work directory
+WORKDIR /usr/src/app
+
+# Copy project files into container
+COPY . .
+
+# Download dependencies
+RUN mvn dependency:resolve
+
+# Default command to run tests (can be overridden)
+CMD ["mvn", "clean", "test"]
--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -1,12 +1,28 @@
 package base;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class BasePage {
     public static void main(String[] args) {
+
         ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
-        System.out.println("BasePage.main");
+        driver.navigate().to("https://mdcomputers.lk");
+
+//        driver.WebDriverWait(driver, Duration.ofSeconds(10));
+//        WebElement el = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Amaan")));
+
+
+        driver.quit();
     }
-
 }
